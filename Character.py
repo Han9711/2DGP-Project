68495cbb991524212132ade2ed1faf63c783d7fb
Han9711 <hcw9711@naver.com>
2021-11-15 19:21:43 +0900

    game_framework

--- Character.py
+++ Character.py
@@ -5,11 +5,11 @@ import game_world
 
 # Character Run Speed
 # fill expressions correctly
-PIXEL_PER_METER = 0
-RUN_SPEED_KMPH = 0
-RUN_SPEED_MPM = 0
-RUN_SPEED_MPS = 0
-RUN_SPEED_PPS = 1
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0 # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 # Character Action Speed
 # fill expressions correctly
@@ -51,10 +51,11 @@ class IdleState:
         pass
 
     def do(character):
-        character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        character.timer -= 1
-        if character.timer == 0:
-            character.add_event(SLEEP_TIMER)
+        pass
+        # character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        # character.timer -= 1
+        # if character.timer == 0:
+        #     character.add_event(SLEEP_TIMER)
 
     def draw(character):
         if character.dir == 1:
@@ -94,33 +95,33 @@ class RunState:
             character.image.clip_draw(int(character.frame) * 46, 0, 46, 50, character.x, character.y)
             # character.image.clip_draw(int(character.frame) * 100, 100, 100, 100, character.x, character.y)
         else:
-            character.image.clip_draw(int(character.frame) * 46, 100, 46, 50, character.x, character.y)
+            character.image.clip_draw(int(character.frame) * 47, 104, 47, 50, character.x, character.y)
             # character.image.clip_draw(int(character.frame) * 100, 0, 100, 100, character.x, character.y)
 
 
-class SleepState:
-
-    def enter(character, event):
-        character.frame = 0
-
-    def exit(character, event):
-        pass
-
-    def do(character):
-        character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-
-    def draw(character):
-        if character.dir == 1:
-            character.image.clip_composite_draw(int(character.frame) * 100, 300, 100, 100, 3.141592 / 2, '', character.x - 25, character.y - 25, 100, 100)
-        else:
-            character.image.clip_composite_draw(int(character.frame) * 100, 200, 100, 100, -3.141592 / 2, '', character.x + 25, character.y - 25, 100, 100)
+# class SleepState:
+#
+#     def enter(character, event):
+#         character.frame = 0
+#
+#     def exit(character, event):
+#         pass
+#
+#     def do(character):
+#         character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+#
+#     def draw(character):
+#         if character.dir == 1:
+#             character.image.clip_composite_draw(int(character.frame) * 100, 300, 100, 100, 3.141592 / 2, '', character.x - 25, character.y - 25, 100, 100)
+#         else:
+#             character.image.clip_composite_draw(int(character.frame) * 100, 200, 100, 100, -3.141592 / 2, '', character.x + 25, character.y - 25, 100, 100)
 
 
 
 next_state_table = {
-    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, SLEEP_TIMER: SleepState, SPACE: IdleState},
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, SPACE: IdleState},            #SLEEP_TIMER: SleepState
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState, SPACE: RunState},
-    SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState, LEFT_UP: RunState, RIGHT_UP: RunState, SPACE: IdleState}
+    # SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState, LEFT_UP: RunState, RIGHT_UP: RunState, SPACE: IdleState}
 }
 
 
